08c795ad89fa32b666e0b70a634fa316a6bf2931
Bilal AlHafidz <91085882+kyuurazz@users.noreply.github.com>
2023-02-02 00:03:39 +0700

    Update

--- ip-checker.py
+++ ip-checker.py
@@ -1,9 +1,17 @@
 import socket
+import requests
+import pprint
+import json
 import os
 
-os.system('cls')
-hostname = input(f'Please enter a domain name : ')
+os.system('clear')
+hostname = input('Please enter a domain name : ')
 ip_address = socket.gethostbyname(hostname)
 
-print(f'\nHostname \t: {hostname}')
-print(f'IP Address \t: {ip_address}\n')
+request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+response = requests.get(request_url)
+geolocation = response.content.decode()
+geolocation = geolocation.split("(")[1].strip(")")
+geolocation = json.loads(geolocation)
+for k,v in geolocation.items():
+	pprint.pprint(str(k) + ' : ' + str(v))
